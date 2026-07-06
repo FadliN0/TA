6f6b635cd92b92dcaa994b31aa605c75772a873f
Fadli Nofrizal <fadlioppo460@gmail.com>
2026-07-06 08:58:52 +0700

    fixing DO style

--- app/dashboard/delivery-orders/[id]/DeliveryOrderDetailClient.tsx
+++ app/dashboard/delivery-orders/[id]/DeliveryOrderDetailClient.tsx
@@ -75,13 +75,6 @@ export default function DeliveryOrderDetailClient({
           }}
         >
 
-          {/* WATERMARK PARTIAL */}
-          {isPartialDelivery && (
-            <div className="absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 border-[8px] border-slate-300 text-slate-300 px-8 py-2 rounded-3xl text-7xl font-black uppercase tracking-widest opacity-20 transform -rotate-[25deg] pointer-events-none z-50 print:opacity-[0.1]">
-              PARTIAL
-            </div>
-          )}
-
           {/* ===== HEADER ===== */}
             
             {/* 1. Logo dipisah dan diletakkan di atas */}
